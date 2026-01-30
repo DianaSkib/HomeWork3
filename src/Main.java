a714007de6a6c7dcea0bb98b674e55b0de6eb77c
Диана <ashkhar.ik@mail.ru>
2026-01-30 22:13:20 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
    public static void main(String[] args) {
 
       byte hamsters = 33;
-   short busyHamsters = 235;
-   int hamstersWeight = 75000;
-   long giraffe = 3550000;
+      short busyHamsters = 235;
+      int hamstersWeight = 75000;
+      long giraffe = 3550000;
 
-   float sleepGiraffe = 5.5384F;
-   double sleepHamsters = 10.5466884689;
+      float sleepGiraffe = 5.5384F;
+      double sleepHamsters = 10.5466884689;
 
-   System.out.println("Значение переменной hamsters" + " с типом byte" + " равно " + hamsters);
+      System.out.println("Значение переменной hamsters" + " с типом byte" + " равно " + hamsters);
       System.out.println("Значение переменной busyHamsters" + " с типом short" + " равно " + busyHamsters);
       System.out.println("Значение переменной hamstersWeight" + " с типом int" + " равно " + hamstersWeight);
       System.out.println("Значение переменной giraffe" + " с типом long" + " равно " + giraffe);
       System.out.println("Значение переменной sleepGiraffe" + " с типом float" + " равно " + sleepGiraffe);
       System.out.println("Значение переменной sleepHamsters" + " с типом double" + " равно " + sleepHamsters);
-      }
+
+      float a = 27.12f;
+      long b = 987678965549L;
+      float c = 2.786f;
+      short d = 569;
+      short e = -159;
+      short f = 27897;
+      byte g = 67;
+
+   }
 }
