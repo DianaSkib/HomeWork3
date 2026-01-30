ca289cf22844e516cb2739a43295ee7af0041b35
Диана <ashkhar.ik@mail.ru>
2026-01-30 21:43:19 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+   public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+      byte hamsters = 33;
+   short busyHamsters = 235;
+   int hamstersWeight = 75000;
+   long giraffe = 3550000;
+
+   float sleepGiraffe = 5.5384F;
+   double sleepHamsters = 10.5466884689;
+
+   System.out.println("Значение переменной hamsters" + " с типом byte" + " равно " + hamsters);
+      System.out.println("Значение переменной busyHamsters" + " с типом short" + " равно " + busyHamsters);
+      System.out.println("Значение переменной hamstersWeight" + " с типом int" + " равно " + hamstersWeight);
+      System.out.println("Значение переменной giraffe" + " с типом long" + " равно " + giraffe);
+      System.out.println("Значение переменной sleepGiraffe" + " с типом float" + " равно " + sleepGiraffe);
+      System.out.println("Значение переменной sleepHamsters" + " с типом double" + " равно " + sleepHamsters);
+      }
 }
