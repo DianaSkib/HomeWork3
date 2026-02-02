059501b7dfb1ad7e95694b5822ae733e4ee9be2f
Диана <ashkhar.ik@mail.ru>
2026-02-02 21:23:09 +0300

    Начальный коммит

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        byte hamsters = 33;
+        short busyHamsters = 235;
+        int hamstersWeight = 75000;
+        long giraffe = 3550000;
+
+        float sleepGiraffe = 5.5384F;
+        double sleepHamsters = 10.5466884689;
+
+        System.out.println("Значение переменной hamsters" + " с типом byte" + " равно " + hamsters);
+        System.out.println("Значение переменной busyHamsters" + " с типом short" + " равно " + busyHamsters);
+        System.out.println("Значение переменной hamstersWeight" + " с типом int" + " равно " + hamstersWeight);
+        System.out.println("Значение переменной giraffe" + " с типом long" + " равно " + giraffe);
+        System.out.println("Значение переменной sleepGiraffe" + " с типом float" + " равно " + sleepGiraffe);
+        System.out.println("Значение переменной sleepHamsters" + " с типом double" + " равно " + sleepHamsters);
 
     }
 }
