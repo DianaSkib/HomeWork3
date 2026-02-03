7f7bb49f047e7eeabb31183db3ada00805d27b38
Диана <ashkhar.ik@mail.ru>
2026-02-03 21:10:11 +0300

    Task 6

--- src/Main.java
+++ src/Main.java
@@ -68,6 +68,37 @@ public class Main {
 
         System.out.println("В школе, где " + allClass +  " классов, нужно " + whitePaint + " банок белой краски и " + brownPaint + " банок коричневой краски.");
 
+        byte allBananas = 5;
+        byte weightOneBananas = 80;
+        short weightBananas = (short) (weightOneBananas * allBananas);
+            System.out.println (weightBananas + " грамм бананов");
+
+        short allMilk = 200;
+        byte weightOneMilk = 105;
+        int weightMilk = (weightOneMilk * 2);
+            System.out.println (weightMilk + " грамм молока");
+
+        byte iceCream = 2;
+        byte oneBriquett = 100;
+        short weightIcecream = (short) (iceCream * oneBriquett);
+            System.out.println (weightIcecream + " грамм мороженного");
+
+
+        byte rawEggs = 4;
+        byte oneEggs = 70;
+        short weightEggs = (short) (rawEggs * oneEggs);
+            System.out.println (weightEggs + " грамм яиц");
+
+
+        short breakfast = (short) (weightBananas + weightMilk + weightIcecream + weightEggs);
+           System.out.println (breakfast + " грамм получился завтрак");
+
+           float kg = 1000f;
+           float breakfastKg = (breakfast / kg);
+        System.out.println (breakfastKg + " кг.");
+
+
+
 
 
 
